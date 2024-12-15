970738c0421c296a486d9a066f3c3e04d0766969
backend6m5_apex <backend6m5@apexglobal.co>
2024-12-15 00:09:25 -0300

    Fix lombok

--- src/main/java/com/luciano/springboot/webflux/hexagonal/architecture/template/hexagonalarchitecturetemplate/application/dto/CreateOrderRequest.java
+++ src/main/java/com/luciano/springboot/webflux/hexagonal/architecture/template/hexagonalarchitecturetemplate/application/dto/CreateOrderRequest.java
@@ -1,31 +1,13 @@
 package com.luciano.springboot.webflux.hexagonal.architecture.template.hexagonalarchitecturetemplate.application.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CreateOrderRequest {
     private String productId;
     private Integer quantity;
-
-    public CreateOrderRequest() {
-    }
-
-    public CreateOrderRequest(String productId, Integer quantity) {
-        this.productId = productId;
-        this.quantity = quantity;
-    }
-
-    public String getProductId() {
-        return productId;
-    }
-
-    public void setProductId(String productId) {
-        this.productId = productId;
-    }
-
-    public Integer getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(Integer quantity) {
-        this.quantity = quantity;
-    }
 }
--- src/main/java/com/luciano/springboot/webflux/hexagonal/architecture/template/hexagonalarchitecturetemplate/infrastructure/entities/OrderDocument.java
+++ src/main/java/com/luciano/springboot/webflux/hexagonal/architecture/template/hexagonalarchitecturetemplate/infrastructure/entities/OrderDocument.java
@@ -1,6 +1,9 @@
 package com.luciano.springboot.webflux.hexagonal.architecture.template.hexagonalarchitecturetemplate.infrastructure.entities;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -8,6 +11,9 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Document("orders")
 public class OrderDocument {
     @Id
@@ -16,56 +22,4 @@ public class OrderDocument {
     private Integer quantity;
     private BigDecimal totalPrice;
     private LocalDateTime createdAt;
-
-    public OrderDocument() {
-    }
-
-    public OrderDocument(String id, String productId, Integer quantity, BigDecimal totalPrice,
-                         LocalDateTime createdAt) {
-        this.id = id;
-        this.productId = productId;
-        this.quantity = quantity;
-        this.totalPrice = totalPrice;
-        this.createdAt = createdAt;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getProductId() {
-        return productId;
-    }
-
-    public void setProductId(String productId) {
-        this.productId = productId;
-    }
-
-    public Integer getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(Integer quantity) {
-        this.quantity = quantity;
-    }
-
-    public BigDecimal getTotalPrice() {
-        return totalPrice;
-    }
-
-    public void setTotalPrice(BigDecimal totalPrice) {
-        this.totalPrice = totalPrice;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
 }
